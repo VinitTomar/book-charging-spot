0e8862a7fcbee8860d01ae4dbd087dec00bf04ef
Vinit Tomar <vinit.tomar68@gmail.com>
2020-11-01 14:51:02 +0530

    environment setup

--- apps/web-apis/src/environments/environment.prod.ts
+++ apps/web-apis/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
   production: true,
+  database: {
+    type: process.env.DB_TYPE,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_DATABASE_NAME,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+  }
 };
--- apps/web-apis/src/environments/environment.ts
+++ apps/web-apis/src/environments/environment.ts
@@ -1,6 +1,11 @@
 export const environment = {
   production: false,
-
-  dbType: 'postgre'
-
+  database: {
+    type: process.env.DB_TYPE,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_DATABASE_NAME,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+  }
 };
